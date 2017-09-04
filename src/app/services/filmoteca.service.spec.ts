af6bcbc11f66070ffb9b186196e2040489d019fa
roberto zazo lopez <roberto.zazolopez@altran.com>
2017-09-04 15:45:10 +0200

    unit-test filmoteca-service

--- src/app/services/filmoteca.service.spec.ts
+++ src/app/services/filmoteca.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { FilmotecaService } from './filmoteca.service';
+import { HttpModule } from '@angular/http';
 
 describe('FilmotecaService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports : [HttpModule],
       providers: [FilmotecaService]
     });
   });
