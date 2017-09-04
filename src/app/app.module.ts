bd65bd651521c87a8e8e63ae9f5854a6659f961c
roberto zazo lopez <roberto.zazolopez@altran.com>
2017-09-04 14:56:44 +0200

    add module & service to get HTTP [series & movies]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HttpCacheModule } from 'ng-http-cache';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PersistenceModule } from 'angular-persistence';
+import { FilmotecaService } from './services/filmoteca.service';
+import { SeriesModule } from './components/series/series.module';
+import { MoviesModule } from './components/movies/movies.module';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,16 @@ import { PersistenceModule } from 'angular-persistence';
   imports: [
     BrowserModule,
     HttpClientModule,
-    PersistenceModule,
-    HttpCacheModule
+    HttpCacheModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent, pathMatch: 'full'},
+      { path: 'movies', loadChildren: './components/movies/movies.module#MoviesModule'},
+      { path: 'series', loadChildren: './components/series/series.module#SeriesModule'}
+    ]),
+    SeriesModule,
+    MoviesModule
   ],
-  providers: [],
+  providers: [FilmotecaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
